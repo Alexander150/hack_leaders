5ddf7829cd1849a1d10dcc775feca8921db115ac
Alexander <zarnik119@gmail.com>
2020-10-31 01:30:04 +0300

    s

--- hack_leaders_client/src/Login.svelte
+++ hack_leaders_client/src/Login.svelte
@@ -1,13 +1,17 @@
 <script>
 	export let params;
 
+	let user = {
+		user_type: params.user_type,
+	}
+
 </script>
 
 <div>
-	{#if params.user == "teacher"}
+	{#if params.user_type == "teacher"}
 		<div>Учитель</div>
 	{/if}
-	{#if params.user == "student"}
+	{#if params.user_type == "student"}
 		<div>Ученик</div>
 	{/if}
 </div>
