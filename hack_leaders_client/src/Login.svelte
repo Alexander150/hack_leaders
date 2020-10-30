02817cf9c90d1b7bbc72bbef15008c645485b351
Alexander <zarnik119@gmail.com>
2020-10-31 01:28:03 +0300

    s

--- hack_leaders_client/src/Login.svelte
+++ hack_leaders_client/src/Login.svelte
@@ -4,7 +4,12 @@
 </script>
 
 <div>
-	{params}
+	{#if params.user == "teacher"}
+		<div>Учитель</div>
+	{/if}
+	{#if params.user == "student"}
+		<div>Ученик</div>
+	{/if}
 </div>
 
 <style type="text/css" media="screen">
