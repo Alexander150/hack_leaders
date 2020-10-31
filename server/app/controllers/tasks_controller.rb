782c65e8bdf2226820d631e732a8e4515a335172
Alexander <zarnik119@gmail.com>
2020-10-31 18:08:11 +0300

    s

--- server/app/controllers/tasks_controller.rb
+++ server/app/controllers/tasks_controller.rb
@@ -1,14 +1,10 @@
 class TasksController < ApplicationController
 	def check
-		# File.open("code.py", "w+") {|file| 
-		# 	file.write(params[:code])
-		# }
 		file = File.open('code.py', 'w+')
 		file.write(params[:code])
 		file.close
 		file1 = File.open('output.txt', 'w+')
-		# file1.write(system('python code.py'))
-		file1.write(exec('python code.py'))
+		file1.write(system('python3 code.py'))
 		file1.close
 	end
 end
