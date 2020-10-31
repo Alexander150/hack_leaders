a57722edd4425bb625c7109881d1c1bbaf2f6eb5
Alexander <zarnik119@gmail.com>
2020-10-31 19:08:45 +0300

    s

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(username: "teacher", password: "password", user_type: "teacher")
-user = User.create(username: "student", password: "password", user_type: "student")
+# user = User.create(username: "teacher", password: "password", user_type: "teacher")
+# user = User.create(username: "student", password: "password", user_type: "student")
+task = Task.create(inputs: "1,2,3,4,5,6,7,8,9,10", outputs: "2,4,6,8,10,12,14,16,18,20")
