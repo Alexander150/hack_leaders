07f11c6f2eb9ed98e78742b3051f91e543c6a35d
kinton <kintonk@yandex.ru>
2020-10-31 19:18:34 +0300

    k

--- server/app/controllers/tasks_controller.rb
+++ server/app/controllers/tasks_controller.rb
@@ -1,10 +1,19 @@
 class TasksController < ApplicationController
 	def check
-		file = File.open('tmp/code.py', 'w+')
+		file = File.open('tmp/their_code.py', 'w')
 		file.print(params[:code])
 		file.close
-		file1 = File.open('tmp/output.txt', 'w+')
-		file1.print(`python3 tmp/code.py `)
+
+		f = File.open('tmp/ours_code.py', 'w')
+		f.print('')
+		f = File.open('tmp/ours_code.py', 'a')
+		f.print("from their_code import f\n")
+		f.print("arr = [1, 2, 3]\n")
+		f.print("print(f(arr))\n")
+		f.close()
+
+		file1 = File.open('tmp/output.txt', 'w')
+		file1.print(`python3 tmp/ours_code.py `)
 		file1.close
 	end
 end
